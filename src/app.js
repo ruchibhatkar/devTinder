9385d37808d414d95c1c59148503c5e01fb7fcb6
Ruchi Bhatkar <bhatkarruchi@gmail.com>
2025-04-22 19:56:16 +0530

    Using Different APIs

--- src/app.js
+++ src/app.js
@@ -7,23 +7,52 @@ const express = require("express");
 const app = express();
 
 //Create request handler functions
-app.use("/hello",(req,res)=>{       // "/hello" are called as routes here
-    res.end("Namste dev");
+// app.use("/hello",(req,res)=>{       // "/hello" are called as routes here
+//     res.end("Namste dev");
+// });
+
+// app.use("/test",(req,res)=>{
+//     res.end("Hello from the test server");
+// });
+
+// app.use("/api/mine",(req,res)=>{
+//     res.end("Hello from the API");
+// });
+
+// app.use("/",(req,res)=>{
+//     res.end("I'm a dashboard");
+// });
+
+app.post("/user",(req,res) => {
+    res.end("Data saved to database successfully");
 });
 
-app.use("/test",(req,res)=>{
-    res.end("Hello from the test server");
+app.get("/user",(req,res) => {
+    res.end("I am GET API");
 });
 
-app.use("/api/mine",(req,res)=>{
-    res.end("Hello from the API");
+app.delete("/user",(req,res) => {
+    res.end("Data deleted successfully");
+}); 
+
+app.put("/user",(req,res) => {
+    res.end("Data updated successfully by PUT API");
 });
 
-app.use("/",(req,res)=>{
-    res.end("I'm a dashboard");
+app.patch("/user",(req,res) => {
+    res.end("Data updated by successfully by PATCH API");
 });
+
+app.use("/user",(req,res) =>{
+    res.end("HAHAHAHAHA");
+});
+
+
 
 //Listen to server
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777");
 });
+
+//npm run dev : for nodemon (provides hot reload)
+//npm run start : for node
